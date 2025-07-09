51102fe01d7a7edd35bd3af4aaa3b45ae31856c2
Prithish Chaudhary <chaudharyprithish05@gmail.com>
2025-07-09 13:50:41 +0530

    Matrix functions implementation

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,3 +1,39 @@
 #pragma once
 #include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <cmath>
 
+class Matrix {
+    private:
+        std::vector<std::vector<double>> A;
+        size_t rows;
+        size_t cols;
+
+    public:
+
+        Matrix(const size_t rows, const size_t cols);
+        Matrix(const std::vector<std::vector<double>>& input);
+
+        double& operator()(size_t row, size_t col);
+        double operator()(size_t row, size_t col) const;
+        size_t nRows() const {return rows;}
+        size_t nCols() const {return cols;}
+
+        Matrix T() const;
+        Matrix operator*(const Matrix& other) const;
+        std::vector<double> operator*(const std::vector<double>& b) const;
+        Matrix operator*(double d) const;
+        Matrix operator+(const Matrix& other) const;
+        Matrix operator-(const Matrix& other) const;
+        void swapRows(size_t i, size_t j);
+
+        std::vector<double> back_sub(const std::vector<double>& b) const;
+        std::vector<double> for_sub(const std::vector<double>& b) const;
+        std::vector<double> solveQR(const std::vector<double>& b) const;
+        std::vector<double> solveLU(const std::vector<double>& b) const;
+        
+
+        double determinant() const;
+        Matrix inverse() const;
+};
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,1 +1,273 @@
 #include "Matrix.hpp"
+
+Matrix::Matrix(const size_t row, const size_t col) : rows(row), cols(col), A(row, std::vector<double>(col, 0.0)) {}
+Matrix::Matrix(const std::vector<std::vector<double>>& input) : rows(input.size()), cols(input[0].size()), A(input) {}
+
+double& Matrix::operator()(size_t row, size_t col) {
+    return A[row][col];
+}
+
+double Matrix::operator()(size_t row, size_t col) const {
+    return A[row][col];
+}
+
+Matrix Matrix::T() const {
+    int r = nRows();
+    int c = nCols();
+
+    Matrix t(c, r);
+
+    for (size_t i {}; i < c; i++)
+    {
+        for (size_t j {}; j < r; j++)
+        {
+            t(i, j) = A[j][i];
+        }
+    }
+
+    return t;
+}
+
+Matrix Matrix::operator*(const Matrix& other) const {
+    size_t r1 = nRows();
+    size_t c1 = nCols();
+
+    size_t r2 = other.nRows();
+    size_t c2 = other.nCols();
+
+    if (c1 != r2)
+    {
+        throw std::invalid_argument("Matrix dimensions incompatible for multiplication.");
+    }
+
+    Matrix m(r1, c2);
+
+    for (size_t i {}; i < r1; i++)
+    {
+        for (size_t j {}; j < c2; j++)
+        {
+            for (size_t k {}; k < c1; k++)
+            {
+                m(i, j) += A[i][k]*other(k, j);
+            }
+        }
+    }
+
+    return m;
+}
+
+std::vector<double> Matrix::operator*(const std::vector<double>& b) const {
+    size_t n = b.size();
+    size_t r = nRows();
+
+    if (n != nCols())
+    {
+        throw std::invalid_argument("Matrix, Vector dimensions incompatible for multiplication.");
+    }
+
+    std::vector<double> res(r);
+
+    for (size_t i {}; i < r; i++)
+    {
+        for (size_t j {}; j < n; j++)
+        {
+            res[i] += A[i][j]*b[j];
+        }
+    }
+
+    return res;
+}
+
+Matrix Matrix::operator*(double d) const {
+    Matrix m(*this);
+
+    const size_t r = nRows();
+    const size_t c = nCols();
+
+    for (size_t i {}; i < r; i++)
+    {
+        for (size_t j {}; j < c; j++)
+        {
+            m(i, j) *= d;
+        }
+    }
+
+    return m;
+}
+
+Matrix Matrix::operator+(const Matrix& other) const {
+    size_t r1 = nRows();
+    size_t c1 = nCols();
+
+    size_t r2 = other.nRows();
+    size_t c2 = other.nCols();
+
+    if ((r1 != r2) || (c1 != c2))
+    {
+        throw std::invalid_argument("Matrix dimensions incompatible for addition.");
+    }
+
+    Matrix m(r1, c1);
+
+    for (size_t i {}; i < r1; i++)
+    {
+        for (size_t j {}; j < c1; j++)
+        {
+            m(i, j) = A[i][j] + other(i, j);
+        }
+    }
+
+    return m;
+}
+
+Matrix Matrix::operator-(const Matrix& other) const {
+    size_t r1 = nRows();
+    size_t c1 = nCols();
+
+    size_t r2 = other.nRows();
+    size_t c2 = other.nCols();
+
+    if ((r1 != r2) || (c1 != c2))
+    {
+        throw std::invalid_argument("Matrix dimensions incompatible for subtraction.");
+    }
+
+    Matrix m(r1, c1);
+
+    for (size_t i {}; i < r1; i++)
+    {
+        for (size_t j {}; j < c1; j++)
+        {
+            m(i, j) = A[i][j] - other(i, j);
+        }
+    }
+
+    return m;
+}
+
+void Matrix::swapRows(size_t i, size_t j) {
+        
+    if (i >= nRows() || j >= nRows()) {
+        throw std::out_of_range("Row index out of bounds in swapRows.");
+    }
+
+    std::swap(A[i], A[j]);
+}
+
+std::vector<double> Matrix::back_sub(const std::vector<double>& b) const {
+    const size_t n = b.size();
+
+    if (n != nRows())
+    {
+        throw std::invalid_argument("invalid, length of input and output dont match.");
+    }
+
+    std::vector<double> x(n);
+    x[n-1] = b[n-1]/A[n-1][n-1];
+
+    for (int i = n-2; i >= 0; i--)
+    {
+        if (std::abs(A[i][i]) < 1e-12) {
+            throw std::runtime_error("Zero pivot encountered in back substitution.");
+        }
+
+        x[i] = b[i];
+
+        for (int j {i+1}; j < n; j++)
+        {
+            x[i] -= A[i][j]*x[j];
+        }
+
+        x[i] = x[i]/A[i][i];
+    }
+
+    return x;
+}
+
+std::vector<double> Matrix::for_sub(const std::vector<double>& b) const {
+    const size_t n = b.size();
+
+    if (n != nRows())
+    {
+        throw std::invalid_argument("invalid, length of input and output dont match.");
+    }
+
+    std::vector<double> x(n);
+    x[0] = b[0]/A[0][0];
+
+    for (size_t i {1}; i < n; i++)
+    {
+        if (std::abs(A[i][i]) < 1e-12) {
+            throw std::runtime_error("Zero pivot encountered in forward substitution.");
+        }
+
+        x[i] = b[i];
+
+        for (size_t j {}; j < i; j++)
+        {
+            x[i] -= A[i][j]*x[j];
+        }
+
+        x[i] = x[i]/A[i][i];
+    }
+
+    return x;
+}
+
+std::vector<double> Matrix::solveQR(const std::vector<double>& b) const {
+
+}
+
+std::vector<double> Matrix::solveLU(const std::vector<double>& b) const {
+    
+    const size_t n = nCols();
+
+    Matrix X_cpy(*this);
+    std::vector<double> b_cpy = b;
+
+    Matrix X_cpyT = X_cpy.T();
+    Matrix XT_X = X_cpyT*X_cpy;
+
+    b_cpy = X_cpyT*b_cpy;
+
+    Matrix L(n, n);
+    for (size_t i {}; i < n; i++)
+    {
+        L(i, i) = 1.0;
+    }
+
+    for (size_t i {}; i < n; i++)
+    {
+        int mInd = i;
+        for (size_t j {i+1}; j < n; j++)
+        {
+            mInd = (fabs(XT_X(mInd, i)) < fabs(XT_X(j, i)) ? j : mInd);
+        }
+
+        if (mInd != i)
+        {
+            XT_X.swapRows(i, mInd);
+            std::swap(b_cpy[i], b_cpy[mInd]);
+            L.swapRows(i, mInd);
+        }
+
+        if (fabs(XT_X(i, i)) < 1e-12)
+        {
+            throw std::runtime_error("zero-pivot.\n");
+        }
+
+        for (size_t j {i+1}; j < n; j++)
+        {
+            L(j, i) = XT_X(j, i)/XT_X(i, i);
+
+            for (size_t k {i}; k < n; k++)
+            {
+                XT_X(j, k) -= L(j, i)*XT_X(i, k);
+            }
+        }
+    }
+
+    std::vector<double> y = L.for_sub(b_cpy);
+    return XT_X.back_sub(y);
+}
+
