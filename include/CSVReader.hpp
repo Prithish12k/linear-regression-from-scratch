28bb9fe2719eb87df043272325213735a0652205
Prithish Chaudhary <chaudharyprithish05@gmail.com>
2025-07-09 18:23:36 +0530

    added CSV reader

--- include/CSVReader.hpp
+++ include/CSVReader.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <utility>
+#include "Matrix.hpp"
+
+class CSVReader {
+public:
+    static Matrix readMatrix(const std::string& filename, bool hasHeader);
+    static std::pair<Matrix, std::vector<double>> readMatrixWithTarget(const std::string& filename, const std::string& target);
+};
